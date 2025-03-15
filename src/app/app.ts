90d424539a547f0ba62c655d0ce24617cc22d585
Mathew Silva <msilva@akaidentity.io>
2025-03-14 21:25:33 -0400

    feat: initial check in

---
+++ src/app/app.ts
@@ -0,0 +1,32 @@
+import type { FastifyInstance } from 'fastify';
+import AutoLoad from '@fastify/autoload';
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/* eslint-disable-next-line */
+export interface AppOptions {}
+
+export async function app(fastify: FastifyInstance, opts: AppOptions) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: join(__dirname, 'plugins'),
+    options: { ...opts },
+  });
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: join(__dirname, 'routes'),
+    options: { ...opts },
+  });
+}
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import Fastify from 'fastify';
+import { app } from './app/app';
+import swagger from '@fastify/swagger';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+// Instantiate Fastify with some config
+const server = Fastify({
+  logger: true,
+});
+
+await server.register(swagger);
+// Register your application as a normal plugin.
+server.register(app);
+
+await server.ready();
+server.swagger();
+
+// Start listening.
+server.listen({ port, host }, (err) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  } else {
+    console.log(`[ ready ] http://${host}:${port}`);
+  }
+});
