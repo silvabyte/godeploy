23dc465c52791d44d8c5574f098b1d25285389a1
Mat Silva <matsilva@hey.com>
2025-10-21 13:20:14 -0400

    chore: more cleanup

--- makefiles/api/build.mk
+++ makefiles/api/build.mk
@@ -9,12 +9,12 @@ api.build: api.docker.build ## Build API Docker image
 
 api.docker.build: ## Build API Docker image for development
 	$(call print_header,Building API Docker image)
-	@docker build -t $(DOCKER_IMAGE):$(DOCKER_TAG) -f $(PROJECT_ROOT)/Dockerfile $(PROJECT_ROOT)
+	@docker build -t $(DOCKER_IMAGE):$(DOCKER_TAG) -f $(PROJECT_ROOT)/apps/api/Dockerfile $(PROJECT_ROOT)
 	$(call print_success,Docker image built: $(DOCKER_IMAGE):$(DOCKER_TAG))
 
 api.docker.build.prod: ## Build API Docker image for production
 	$(call print_header,Building API Docker image for production)
-	@docker build -t $(DOCKER_IMAGE):$(VERSION) -f $(PROJECT_ROOT)/Dockerfile $(PROJECT_ROOT)
+	@docker build -t $(DOCKER_IMAGE):$(VERSION) -f $(PROJECT_ROOT)/apps/api/Dockerfile $(PROJECT_ROOT)
 	@docker tag $(DOCKER_IMAGE):$(VERSION) $(DOCKER_IMAGE):latest
 	$(call print_success,Docker image built: $(DOCKER_IMAGE):$(VERSION))
 
